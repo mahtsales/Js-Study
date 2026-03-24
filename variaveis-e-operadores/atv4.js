0f210fddd1ed8521ebbf8e3e2e1e7b85b2c015f8
mahtsales <davimatheusfsales@gmail.com>
2026-03-24 11:34:33 -0400

    Feat: Add average grade if-else exercise

---
+++ variaveis-e-operadores/atv4.js
@@ -0,0 +1,22 @@
+/* Faca um algoritmo que dado as 3 notas tiradas por um aluno em um semestre da faculdade. Calcule e imprima
+a sua media e sua classificacao, conforme abaixo:
+    - Media menor que 5 = reprovacao;
+    - Media entre 5 e 7 = recuperacao;
+    - Media acima de 7 = aprovacao;
+*/
+
+const prompt = require("prompt-sync")();
+
+const nota1 = Number(prompt("Digite sua 1º nota: "));
+const nota2 = Number(prompt("Digite sua 2º nota: "));
+const nota3 = Number(prompt("Digite sua 3º nota: "));
+
+const media = (nota1 + nota2 + nota3) / 3;
+
+if(media > 7){
+    console.log(`Media do Aluno: ${media} --- Aluno Aprovado.`);
+}else if(media >= 5 && media < 7){
+    console.log(`Media do aluno: ${media} --- Aluno em Recuperação.`);
+}else{
+    console.log(`Media do aluno: ${media} --- Aluno Reprovado.`);
+};
