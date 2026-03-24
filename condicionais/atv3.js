0ce9da2003043273da772cffe6c0d1bf177288d9
mahtsales <davimatheusfsales@gmail.com>
2026-03-24 00:19:48 -0400

    Fix: Bug fixes and code improvements

--- condicionais/atv3.js
+++ condicionais/atv3.js
@@ -5,6 +5,8 @@ const prompt = require("prompt-sync")();
 const alunoMatriculado = prompt("Você possui nº de matrícula?: ");
 if(alunoMatriculado.toLowerCase() === "sim"){
     console.log("Aluno matriculado.");
-}else{
+}else if (alunoMatriculado.toLowerCase() === "não"){
     console.log("Aluno não matriculado.");
+}else{
+    console.log("Nome não correspondente.");
 };
