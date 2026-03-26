c3c78873919f135a5be8e13412d5ebe7ec7e4854
mahtsales <davimatheusfsales@gmail.com>
2026-03-25 22:08:38 -0400

    feat: add functions exercise

---
+++ funcoes/atv2.js
@@ -0,0 +1,16 @@
+//funcao => pequeno trecho de codigo que podemos invocar sua execucao em qualquer momento, podendo ser utilizado varias vezes.
+
+function contador (valor1, valor2){
+    return valor1 + valor2;
+}
+
+const soma = contador(4,8); //declaramos a variavel fora da funcao, e assim, realizamos a sua invocacao.
+console.log(soma);
+
+function decretarJuros(valor, percentualjuros){
+    return (percentualjuros / 100) * valor
+}
+
+const valorDeJuros = decretarJuros(500, 30);
+console.log(valorDeJuros);
+
